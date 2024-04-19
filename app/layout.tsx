fc3a2c8824d60bad3bef74e6bbd3ac812cb31320
Julia <jules.rechi@gmail.com>
2024-04-19 18:05:17 +0200

    Add footer to all pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,16 @@ export default function RootLayout({ children }) {
           </div>
         </header>
         <main>{children}</main>
+        <footer>
+          <div className="textFooter">European Real Estate Company</div>
+          <div>
+            <span className="textFooter">Contact: </span> erec@erec.com
+          </div>
+          <div>
+            <span className="textFooter">Address:</span> Building Street 123,
+            4567 Vienna
+          </div>
+        </footer>
       </body>
     </html>
   );
